f95f2bdc3ad681f85d90bb5cc47f5293d7b3a5b4
Alfred Xiao <alfred.xiao@qantashotels.com>
2023-05-26 13:59:42 +1000

    describe bytes

--- src/main/java/xiaoyf/demo/schemaregistry/avro/encoding/SimpleTypesDemo.java
+++ src/main/java/xiaoyf/demo/schemaregistry/avro/encoding/SimpleTypesDemo.java
@@ -1,15 +1,14 @@
 package xiaoyf.demo.schemaregistry.avro.encoding;
 
 import org.apache.avro.Schema;
-import org.apache.avro.Schema.Parser;
 import org.apache.avro.SchemaBuilder;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import xiaoyf.demo.schemaregistry.helper.Logger;
 
 import static xiaoyf.demo.schemaregistry.avro.Utilities.bytesToGenericRecord;
-import static xiaoyf.demo.schemaregistry.avro.Utilities.logBytesHex;
 import static xiaoyf.demo.schemaregistry.avro.Utilities.genericRecordToBytes;
+import static xiaoyf.demo.schemaregistry.avro.Utilities.logBytesHex;
 import static xiaoyf.demo.schemaregistry.avro.Utilities.stringToSchema;
 
 public class SimpleTypesDemo {
@@ -56,14 +55,18 @@ public class SimpleTypesDemo {
         Schema schema = stringToSchema(SCHEMA);
 
         GenericRecord record = new GenericData.Record(schema);
-        record.put("f1", 1);     // 02
-        record.put("f2", "XY");  // 04 58 59 (04->2 as length)
-        record.put("f3", 2L);    // 04
-        // 06
+        record.put("f1", 1);     // 02       which is the first byte in output bytes
+        record.put("f2", "XY");  // 04 58 59 (04 means 2, which is length of the string)
+        record.put("f3", 2L);    // 04       which means 2 here
+
         record.put("f4", new GenericData.EnumSymbol(SchemaBuilder.enumeration("Suit").symbols("SPADES", "HEARTS", "DIAMONDS", "CLUBS"), "CLUBS"));
+                                 // 06             which means 3 (index 3, which is the fourth element)
         record.put("f5", null);  // 00             (0 as index in the union types)
-        record.put("f6", "XYZ"); // 02 06 58 59 5A (02 zigzag maps to 1, so 1 is index in the union types)
+        record.put("f6", "XYZ"); // 02 06 58 59 5A (02 means 1, so 1 is index in the union types; 06 means 3, string length is 3)
         record.put("f7", "");    // 00 -> zigzag 0 (string length is 0)
+
+        // above used zigzag numbering scheme: (0 = 0, -1 = 1, 1 = 2, -2 = 3, 2 = 4, -3 = 5, 3 = 6 ...)
+        // where for example 4 is encoded as 2
 
         Logger.log("Avro Record: " + record);
 
@@ -80,8 +83,8 @@ public class SimpleTypesDemo {
 - int/long encoding is using some zig-zag technique, e.g. 1 encoded as 02, -1 as 03, 2 as 04, etc. this is to save space?
 - bytes are assembled one field after another, while the schema is the recipe (for both encoding and decoding)
 
- ## Avro Record: {"f1": 1, "f2": "XY", "f3": 2, "f4": "CLUBS", "f5": null, "f6": "XYZ", "f7": ""}
+ ## Avro Record:            {"f1": 1, "f2": "XY", "f3": 2, "f4": "CLUBS", "f5": null, "f6": "XYZ", "f7": ""}
  ## BYTES: 02 04 58 59 04 06 00 02 06 58 59 5A 00
- ## record read from bytes:{"f1": 1, "f2": "XY", "f3": 2, "f4": "CLUBS", "f5": null, "f6": "XYZ", "f7": ""}
+ ## record read from bytes: {"f1": 1, "f2": "XY", "f3": 2, "f4": "CLUBS", "f5": null, "f6": "XYZ", "f7": ""}
 
  */
